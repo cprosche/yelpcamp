ae6017612524b78187935c112d5d21ab4bf87532
Cade Rosche <caderosche@gmail.com>
2021-10-31 16:43:26 -0500

    finished video 412

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// setup
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
@@ -16,17 +17,35 @@ db.once("open", () => {
 
 const app = express();
 
+// setting up view
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({extended: true}));
+
+// routing
 app.get("/", (req, res) => {
     res.render("home");
 })
 
-app.get("/make-campground", async (req, res) => {
-    const camp = new Campground({title: "My Backyard", description: "Cheap camping!"})
-    await camp.save();
-    res.send(camp);
+app.get("/campgrounds", async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', {campgrounds});
+})
+
+app.get("/campgrounds/new", (req, res) => {
+    res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground (req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.get("/campgrounds/:id", async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', {campground});
 })
 
 app.listen(3000, () => {
