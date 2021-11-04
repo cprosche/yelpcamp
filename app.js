2f74c26eeb937185262324c42505e192f8ce87d2
Cade Rosche <caderosche@gmail.com>
2021-11-03 20:26:19 -0500

    finished video 425

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
 const Campground = require("./models/campground");
+const methodOverride = require("method-override");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
     useNewUrlParser: true,
@@ -18,10 +20,12 @@ db.once("open", () => {
 const app = express();
 
 // setting up view
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 // routing
 app.get("/", (req, res) => {
@@ -48,6 +52,23 @@ app.get("/campgrounds/:id", async (req, res) => {
     res.render('campgrounds/show', {campground});
 })
 
+app.get("/campgrounds/:id/edit", async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', {campground});
+})
+
+app.put("/campgrounds/:id", async (req, res) => {
+    const campground = await Campground.findByIdAndUpdate(req.params.id, {...req.body.campground});
+    res.redirect(`/campgrounds/${campground._id}`);
+})
+
+app.delete("/campgrounds/:id", async (req, res) => {
+    const {id} = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect("/campgrounds");
+})
+
+// this stays at the bottom
 app.listen(3000, () => {
     console.log("Live on port 3000");
 })
